10db24fc755c6d31c11e8056039e0309c9919bad
dgamefather <dgamefather@thatoneplace.net>
2019-11-20 14:35:50 -0700

    New exercise for adding testAssets to tests themselves.

---
+++ tests/manager.js
@@ -0,0 +1,30 @@
+var edit = require('../testAssets/employee-edit');
+var employee = {};
+module.exports = {
+    beforeEach: browser => {
+
+        // Info
+        browser.psych = {
+            name: 'Shawn Spencer',
+            phone: '8015556789',
+            title: 'Ive heard it both ways.'
+        };
+
+        employee = browser.page.employeePage();
+        employee
+            .navigate();
+    },
+    after: browser => {
+        browser
+            .end();
+    },
+    'Add New Employee': browser => {
+        edit(employee, 'Bernice Ortiz',
+            {
+                name: `${browser.psych.name}`,
+                phone: `${browser.psych.phone}`,
+                title: `${browser.psych.title}`
+            },
+            'Phillip Weaver');
+    }
+}
