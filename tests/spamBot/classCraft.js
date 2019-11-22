ad5f153765c997eda4730aecf90af73cb88ca60a
dgamefather <dgamefather@thatoneplace.net>
2019-11-22 11:14:52 -0700

    Fixed name for slackSpam script and implimented new concepts to allow logins to be secure!

--- tests/spamBot/classCraft.js
+++ tests/spamBot/classCraft.js
@@ -1,32 +1,32 @@
+var craft = {};
+var loginInfo = require('../../testAssets/logins/andrew');
 module.exports = {
     before: browser => {
-        browser.url("https://game.classcraft.com/")
+        craft = browser.page.classPage()
+        craft
+            .navigate();
     },
     after: browser => {
-        browser.end()
+        browser
+        .end();
     },
     'Login': browser => {
-        browser
-            .pause(3000)
-            .click('.Login__toggle-button-wrap')
-            .pause(1000)
-            .setValue('[name="username"]', ['fng_pewdrew'])
-            .pause(3000)
-            .setValue('[name="password"]', ['ILoveMom!', browser.Keys.ENTER])
+        craft
+            .login(loginInfo);
     },
-    'SpamBot': browser => {
-        browser
+    'SpamBot': browser => { // BROKEN!
+        craft
+            .click('@messanger')
             .pause(3000)
-            .url('https://game.classcraft.com/student/class/TKR6FowWthvbMRGrG/messaging')
-            .pause(3000)
-            .useXpath()
-            .click('//div[contains(text(), "Mr. Gardner Gardner")]')
-            .useCss()
-            for(var x = 0; x <= 20; x++ )
-            {
-                browser
-                    .setValue('[name="newComment"]', ['sPaMeR iS hErE! :)', browser.Keys.ENTER])
-            }
-            browser.pause()
-    }
+            .click('@guard')
+            .waitForElementPresent('@text')
+        for (var x = 0; x <= 20; x++) {
+            craft
+                // .setValue('@text', ['sPaMeR iS hErE! :)', browser.Keys.ENTER])
+                .setValue('@text', 'sPaMeR iS hErE! :)')
+                .pause()
+        }
+        // craft
+        //     .pause();
+    },
 }
---
+++ tests/spamBot/slackSpam.js
@@ -0,0 +1,77 @@
+var slack = {};
+var loginInfo = require('../../testAssets/logins/slack');
+module.exports = {
+    before: browser => {
+        slack = browser.page.slackPage()
+        slack
+            .navigate();
+    },
+    after: browser => {
+        browser
+            .end();
+    },
+
+    'Login': browser => { // COMMENT OUT TO SHOW MERCY
+        slack
+            .login(loginInfo);
+    },
+
+    // TEST ON MYSELF
+    'SpamBot - Myself': browser => {
+        slack
+            .click('@test')
+            .waitForElementPresent('@text');
+        for (var x = 0; x <= 2; x++) {
+            slack
+                .setValue('@text', [`TEST SPAM ${x} HERE!`, browser.Keys.ENTER]);
+        }
+    },
+
+    // Mars
+    'SpamBot - Mars': browser => {
+        slack
+            .click('@mars')
+            .waitForElementPresent('@text')
+            // .setValue('@text', ['5 minute SPAM WARNING! TURN ON DO NOT DISTURB ASAP!!!!! Have a nice day. ;)', browser.Keys.ENTER])
+            // .pause(300000);
+            .setValue('@text', ['2 minute SPAM WARNING! Only sending 2-4 msgs to test new concepts.', browser.Keys.ENTER])
+            .pause(120000);
+        for (var x = 0; x <= 2; x++) {
+            slack
+                .setValue('@text', ['sPaMeR iS hErE! ;)', browser.Keys.ENTER]);
+        }
+        slack
+            // .setValue('@text', ['Your thoughts?', browser.Keys.ENTER])
+            .setValue('@text', ['Check my repo for code if you want. Not including login file for security reasons. https://github.com/dgamefather/DevMtn-Tests', browser.Keys.ENTER])
+            // .pause();
+    },
+
+    // Drey
+    'SpamBot - Drey': browser => {
+        browser
+            .click('@drey')
+            .waitForElementPresent('@text')
+            .setValue('@text', ['Only sending 2-4 msgs to test new concepts. Will delete after test is successful', browser.Keys.ENTER]);
+        for (var x = 0; x <= 2; x++) {
+            slack
+                .setValue('@text', ['sPaMeR iS hErE! ;)', browser.Keys.ENTER]);
+        }
+        slack
+            .setValue('@text', ['Check my repo for code if you want. Not including login file for security reasons. https://github.com/dgamefather/DevMtn-Tests', browser.Keys.ENTER])
+            // .pause();
+    },
+
+    // Sam
+    'SpamBot - Sam': browser => {
+        slack
+            .click('@sam')
+            .waitForElementPresent('@text')
+            .setValue('@text', ['Only sending 2-4 msgs to test new concepts. Will delete after test is successful', browser.Keys.ENTER]);
+        for (var x = 0; x <= 2; x++) {
+            browser
+                .setValue('@text', ['Spare Rations Curated is best! LUL', browser.Keys.ENTER]);
+        }
+        // slack
+        //     .pause();
+    },
+}
