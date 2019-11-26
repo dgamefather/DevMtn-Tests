044f2cb1d756d2649201a2346ad5f79d4445e99c
dgamefather <dgamefather@thatoneplace.net>
2019-11-26 15:43:35 -0700

    Template now is up to date

--- templatePageObj.js
+++ templatePageObj.js
@@ -1,9 +1,18 @@
+var sampleCommands = {
+    sample: function (param) {
+        this
+            .somethingHere();
+        return this;
+    },
+}
 module.exports = {
     url: 'https://thisurl.com',
+    commands: [sampleCommands],
     elements: {
-        nameOfSelector: {
+        nameOfXpathSelector: {
             selector: ('selector'),
             locateStrategy: 'xpath'
         },
+        nameOfCssSelector: '',
     }
 }
